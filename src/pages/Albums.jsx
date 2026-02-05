51e3a6347c65a46764457ac9846534e4a4d97517
alexandre.philip9148@gmail.com <alexandre.philip9148@gmail.com>
2026-02-05 15:10:57 +0100

    Create working Albums modal

--- src/pages/Albums.jsx
+++ src/pages/Albums.jsx
@@ -139,7 +139,7 @@ const Albums = () => {
         )}
 
         {selectedPhoto && (
-          <div className="fixed inset-0 bg-black/80 z-50 flex items-center justify-center p-4">
+          <div className="fixed inset-0 bg-black/80 z-50 flex items-center justify-center p-4" onClick={() => setSelectedPhoto(null)}>
             <div className="bg-white dark:bg-museum-800 rounded-lg max-w-4xl max-h-[90vh] overflow-hidden relative">
               <button
                 onClick={() => setSelectedPhoto(null)}
